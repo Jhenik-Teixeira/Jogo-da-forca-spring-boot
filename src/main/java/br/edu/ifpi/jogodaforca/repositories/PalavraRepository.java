693fd6713975ab9b6ec903368140865a0cf4714f
Jhenik_Brito <jheniktbrito@gmail.com>
2025-02-09 11:45:51 -0300

    camada de repositorios

---
+++ src/main/java/br/edu/ifpi/jogodaforca/repositories/PalavraRepository.java
@@ -0,0 +1,14 @@
+package br.edu.ifpi.jogoforca.repositories;
+
+import br.edu.ifpi.jogoforca.models.Palavra;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PalavraRepository extends JpaRepository<Palavra, Long> {
+
+    // Modificando a query para retornar apenas uma palavra aleatória
+    @Query(value = "SELECT * FROM palavra ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    Palavra findRandomPalavra();
+}
