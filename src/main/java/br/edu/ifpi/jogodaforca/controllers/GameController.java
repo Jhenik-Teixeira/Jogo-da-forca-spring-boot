943f7306d2c61128eafcb1f5eda0197865a16b53
Jhenik_Brito <jheniktbrito@gmail.com>
2025-02-09 14:40:35 -0300

    classe controladora login

--- src/main/java/br/edu/ifpi/jogodaforca/controllers/GameController.java
+++ src/main/java/br/edu/ifpi/jogodaforca/controllers/GameController.java
@@ -5,10 +5,8 @@ import br.edu.ifpi.jogodaforca.services.GameService;
 import br.edu.ifpi.jogodaforca.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/jogo")
@@ -19,14 +17,19 @@ public class GameController {
     private UsuarioService usuarioService;
 
     @GetMapping
-    public String jogo(Model model) {
+    public String jogo(@RequestParam(required = false) String nickname, Model model) {
+        if (nickname == null || nickname.trim().isEmpty()) {
+            return "redirect:/";
+        }
+
         Palavra palavra = gameService.getRandomPalavra();
         model.addAttribute("palavra", palavra);
         model.addAttribute("topScores", usuarioService.getTopScores());
+        model.addAttribute("nickname", nickname);
         return "jogo";
     }
 
-    @PostMapping("/resultado")
+    @PostMapping("/salvar-pontuacao")
     @ResponseBody
     public void salvarPontuacao(@RequestParam String nickname, @RequestParam int pontuacao) {
         usuarioService.saveOrUpdateUsuario(nickname, pontuacao);
